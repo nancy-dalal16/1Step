bfd3e9c44487107de2d826b001199f4a2f1eca5c
nancy-dalal16 <nancydalal16@gmail.com>
2025-05-23 23:56:56 +0530

    new page 1step app

--- assets/js/home.js
+++ assets/js/home.js
@@ -62,3 +62,83 @@ $("#framework-factors").owlCarousel({
     },
   },
 });
+
+$("#green-slide").owlCarousel({
+  loop: false,
+  margin: 0,
+  responsiveClass: true,
+  autoplay: false,
+  nav: true,
+  navText: [
+    "<img src='../assets/img/1StepApp/dark-left-arrow.png'>",
+    "<img src='../assets/img/1StepApp/dark-right-arrow.png''>",
+  ],
+  dots: true,
+  animateIn: "fadeIn",
+  animateOut: "fadeOut",
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+    1200: {
+      items: 1,
+    },
+  },
+});
+
+$("#healthy-habits").owlCarousel({
+  loop: false,
+  margin: 0,
+  responsiveClass: true,
+  autoplay: false,
+  nav: false,
+  dots: true,
+  animateIn: "fadeIn",
+  animateOut: "fadeOut",
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+    1200: {
+      items: 1,
+    },
+  },
+});
+$("#testi").owlCarousel({
+  loop: false,
+  margin: 0,
+  responsiveClass: true,
+  autoplay: false,
+  nav: true,
+  navText: [
+    "<img src='../assets/img/1StepApp/light-left-arrow.png'>",
+    "<img src='../assets/img/1StepApp/light-right-arrow.png''>",
+  ],
+  dots: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+    1200: {
+      items: 1,
+    },
+  },
+});
